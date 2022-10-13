78e44ab13b31ce677e1e3c5698324ddeed0906a0
MaxwellChrist <ribthulu@gmail.com>
2022-10-13 14:13:55 -0400

    read through leaflet docs; installed necessary files to work with react and added starter map file; added ids to leaflet components in order for them to work and be styled appropriately in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
-function App() {
+export default function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +14,19 @@ function App() {
           </form>
         </div>
       </header>
+      <div id="leaflet-container">
+      <MapContainer id="map-container" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      </MapContainer>
+      </div>
       <div className="attribution">
         <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>.</p> 
         <p>Coded by <a href="https://github.com/MaxwellChrist/IP">Max Christ</a>.</p>
@@ -19,5 +34,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
