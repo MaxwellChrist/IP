2b6cb498f74be3ddc80b3c2823ff1b4043c5b18b
MaxwellChrist <ribthulu@gmail.com>
2022-10-13 11:05:30 -0400

    organized project files with create react app starting files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Testing...</h1>
+        <ul>
+          <li>IP Address Tracker</li>
+          <li>Search for any IP address or domain</li>
+          <li>IP Address</li>
+          <li>Location</li>
+          <li>Timezone</li>
+          <li>UTC add offset value dynamically using the API</li>
+          <li>ISP</li>
+        </ul>
       </header>
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/MaxwellChrist/IP">Max Christ</a>.
+      </div>
     </div>
   );
 }
