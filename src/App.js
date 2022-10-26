b3a2a0b8853fdb94791c7abae877fc9cf51602a8
MaxwellChrist <ribthulu@gmail.com>
2022-10-26 15:13:18 -0400

    removed unnecessary component file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
 import React, { useState, useEffect } from 'react'
 import SearchInfo from './Components/SearchInfo/SearchInfo';
 import Map from './Components/Map/Map';
@@ -13,24 +10,6 @@ export default function App() {
   let [marker, setMarker] = useState([51.505, -0.09])
   let [error, SetError] = useState(null)
 
-  async function IpAddressFinder() {
-    let endpoint = `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_apiKey}&ipAddress=${address}`
-    try {
-      const response = await fetch(endpoint);
-      console.log(response)
-      if(response.ok){
-        const jsonResponse = await response.json()
-        setData(jsonResponse)
-      } else {
-        SetError(response.statusText)
-      }
-    }
-    catch(error){
-      console.log(error)
-      SetError(error.messages)
-    }
-  }
-
   function userSubmit(e){
     e.preventDefault()
     let userInput = document.getElementById("user-input")
@@ -38,15 +17,28 @@ export default function App() {
   }
 
   useEffect(() => {
+    async function IpAddressFinder() {
+      let endpoint = `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_apiKey}&ipAddress=${address}`
+      try {
+        const response = await fetch(endpoint);
+        console.log(response)
+        if(response.ok){
+          const jsonResponse = await response.json()
+          setData(jsonResponse)
+        } else {
+          SetError(response.statusText)
+        }
+      }
+      catch(error){
+        console.log(error)
+        SetError(error.messages)
+      }
+    }
     IpAddressFinder()
-  },[address])
+  },[address, setMarker])
 
   useEffect(() => {
     if (data !== null) {
-      console.log(data.ip)
-      console.log(`${data.location.city}, ${data.location.region}`)
-      console.log(data.location.timezone)
-      console.log(data.isp)
       setMarker([data.location.lat, data.location.lng])
     }
   },[data])
--- src/Components/SearchInfo/SearchInfo.js
+++ src/Components/SearchInfo/SearchInfo.js
@@ -4,22 +4,24 @@ import './SearchInfo.css'
 export default function SearchInfo({data}) {
     return (
         <>
-            <div className="ip-results">
-                <div className="ip-results-item">
-                    <h3 className="ip-results-item-heading">IP Address</h3>
-                    <h3 className="ip-results-item-data">{data.ip}</h3>
-                </div>
-                <div className="ip-results-item">
-                    <h3 className="ip-results-item-heading">Location</h3>
-                    <h3 className="ip-results-item-data">{data.location.city + data.location.region}</h3>
-                </div>
-                <div className="ip-results-item">
-                    <h3 className="ip-results-item-heading">Timezone</h3>
-                    <h3 className="ip-results-item-data">{data.location.timezone}</h3>
-                </div>
-                <div className="ip-results-item">
-                    <h3 className="ip-results-item-heading">ISP</h3>
-                    <h3 className="ip-results-item-data">{data.isp}</h3>
+            <div id="ip-container">
+                <div className="ip-results">
+                    <div className="ip-results-item">
+                        <h3 className="ip-results-item-heading">IP Address</h3>
+                        <h3 className="ip-results-item-data">{data.ip}</h3>
+                    </div>
+                    <div className="ip-results-item">
+                        <h3 className="ip-results-item-heading">Location</h3>
+                        <h3 className="ip-results-item-data">{data.location.city + data.location.region}</h3>
+                    </div>
+                    <div className="ip-results-item">
+                        <h3 className="ip-results-item-heading">Timezone</h3>
+                        <h3 className="ip-results-item-data">{data.location.timezone}</h3>
+                    </div>
+                    <div className="ip-results-item">
+                        <h3 className="ip-results-item-heading">ISP</h3>
+                        <h3 className="ip-results-item-data">{data.isp}</h3>
+                    </div>
                 </div>
             </div>
         </>
